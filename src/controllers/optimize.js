65d2aa93a153553f1426c1c72f942bd3767a331c
Stablest <marlonlocci@hotmail.com>
2023-06-22 20:12:38 -0300

    bug-fix: Throws error when body is not found in request

--- src/controllers/optimize.js
+++ src/controllers/optimize.js
@@ -1,23 +1,24 @@
 const sharp = require("sharp");
+const BadRequestError = require("../errors/bad-request-error");
 
-const optimizeImage = async (req, res) => {
-  if (!req.body) return res.status(400).json({ error: "No body was found" });
-
-  let { width, height, fit, type, quality } = req.query;
-  width = width && Number.parseInt(width);
-  height = height && Number.parseInt(height);
-  quality = quality && Number.parseInt(quality);
-
+const optimizeImage = async (req, res, next) => {
   try {
+    if (Object.keys(req.body).length === 0)
+      throw new BadRequestError("Please provide a valid body: image file");
+
+    let { width, height, fit, type, quality } = req.query;
+    width = width && Number.parseInt(width);
+    height = height && Number.parseInt(height);
+    quality = quality && Number.parseInt(quality);
+
     const optImage = await sharp(req.body)
       .resize(width, height, { fit: fit })
       .toFormat(type ?? "input", { quality: quality })
       .toBuffer();
     res.set("Content-type", `image/${type}`);
     return res.status(200).send(optImage);
-  } catch (e) {
-    console.error(e);
-    return res.status(400).json({ error: e.toString() });
+  } catch (err) {
+    next(err);
   }
 };
 
