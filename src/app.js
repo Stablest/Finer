28414efd97b71e954ddb682379f4935f33f95098
Stablest <marlonlocci@hotmail.com>
2023-06-22 19:40:12 -0300

    refactor: Removed app.listen from app.js and added to server.js

--- src/app.js
+++ src/app.js
@@ -16,16 +16,4 @@ app.use(notFound);
 
 // App
 
-const port = process.env.PORT || 5000;
-
-const start = async () => {
-  try {
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-start();
+module.exports = { app };
