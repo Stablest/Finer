5dad74be787df3105b9363f13fdf0d6058467bf5
Stablest <marlonlocci@hotmail.com>
2023-06-22 20:11:56 -0300

    feat: Added errorHandler middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const notFound = require("./middleware/not-found");
 const app = express();
 const optimize = require("./routes/optimize");
+const { errorHandler } = require("./middleware/error-handler");
 
 app.use(express.raw({ type: "image/*", limit: "50mb" }));
 
@@ -13,6 +14,7 @@ app.use("/api/v1/optimize", optimize);
 // Error
 
 app.use(notFound);
+app.use(errorHandler);
 
 // App
 
